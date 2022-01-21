b50d5987e137c7206af8df9f1d37e79cdd0afaec
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2022-01-20 23:26:33 -0500

    updated example

--- examples/load_example.jl
+++ examples/load_example.jl
@@ -3,7 +3,7 @@ using HPD
 
 path = joinpath(dirname(pathof(HPD)), "..", "examples")
 domain = load_domain(joinpath(path, "domain.hpd"))
-# problem = load_problem(joinpath(path, "problem.hpd"))
-# state = initstate(domain, problem)
-# goal = goalstate(domain, problem)
+problem = load_problem(joinpath(path, "problem.hpd"))
+state = initstate(domain, problem)
+goal = goalstate(domain, problem)
 1
